90929146209188883d8188a4cc9d58976c805230
JACKIE LEUNG (MIS-ISD-OOCLL/HKG) <LEUNGJA3@OOCL.COM>
2020-11-25 15:22:34 +0800

    [Jackie] finished Practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,33 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+        this.klass = null;
+    }
+
+    public Teacher(String name,int age, Klass klass ) {
+        super(name,age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return this.klass;
+    }
+
+    public String introduce() {
+        String intro = super.introduce() + " I am a Teacher.";
+        if(klass != null)
+            return intro + " I teach Class " + klass.getNumber() + ".";
+        else return intro + " I teach No Class.";
+    }
+
+    public String introduceWith(Student student) {
+        if(student.getKlass().getNumber() == klass.getNumber()) {
+            return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+        }
+        else  return super.introduce() + " I am a Teacher. I don't teach " + student.getName() + ".";
+    }
 }
