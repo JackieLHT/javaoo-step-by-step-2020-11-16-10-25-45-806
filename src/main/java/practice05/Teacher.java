476d6500103f78de11a4ccba42f9e8946c9ee076
JACKIE LEUNG (MIS-ISD-OOCLL/HKG) <LEUNGJA3@OOCL.COM>
2020-11-25 15:09:13 +0800

    [Jackie] finished Practice05

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,26 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Integer klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+        this.klass = null;
+    }
+
+    public Teacher(String name,int age, int klass ) {
+        super(name,age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return this.klass;
+    }
+
+    public String introduce() {
+        String intro = super.introduce() + " I am a Teacher.";
+        if(klass != null)
+            return intro + " I teach Class " + klass + ".";
+        else return intro + " I teach No Class.";
+    }
 }
